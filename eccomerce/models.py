36d91a6f360b595d5ea7dfabce4431ddfa2feae5
LizandroBackend <codeversebackend@gmail.com>
2024-06-16 12:08:42 -0600

    ci:Add the models and applied migrations to the project | Dev: Lizandro

--- eccomerce/models.py
+++ eccomerce/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+ 
+class Category(models.Model): 
+    name = models.CharField(max_length=200) 
+    description = models.TextField() 
+     
+    def __str__(self): 
+        return self.name 
+     
+class Product(models.Model): 
+    name = models.CharField(max_length=200) 
+    description = models.TextField() 
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    stock = models.IntegerField() 
+    available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE) 
+
+    def __str__(self): 
+        return self.name 
