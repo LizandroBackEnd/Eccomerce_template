2fb0c6eef3570f2647e2983276e2f487c836f1fd
LizandroBackend <codeversebackend@gmail.com>
2024-06-17 20:10:24 -0600

    ci: I setup permissions and authenticated to the files views and settings | Dev: Lizandro

--- eccomerce/views.py
+++ eccomerce/views.py
@@ -1,19 +1,24 @@
 from rest_framework import generics 
 from .models import Category, Product 
 from .serializers import CategorySerializer, ProductSerializer 
+from rest_framework.permissions import IsAuthenticated 
  
 class CategoryList(generics.ListCreateAPIView): 
     queryset = Category.objects.all() 
-    serializer_class = CategorySerializer 
+    serializer_class = CategorySerializer  
+    permission_classes = [IsAuthenticated]
      
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView): 
     queryset = Category.objects.all() 
-    serializer_class = CategorySerializer 
+    serializer_class = CategorySerializer  
+    permission_classes = [IsAuthenticated]
      
 class ProductList(generics.ListCreateAPIView): 
     queryset = Product.objects.all() 
     serializer_class = ProductSerializer 
+    permission_classes = [IsAuthenticated] 
      
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView): 
     queryset = Product.objects.all() 
-    serializer_class = ProductSerializer
+    serializer_class = ProductSerializer 
+    permission_classes = [IsAuthenticated]
--- server/settings.py
+++ server/settings.py
@@ -39,7 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles', 
     'rest_framework', 
     'eccomerce',
-]
+] 
+ 
+REST_FRAMEWORK = { 
+    'DEFAULT_AUTHENTICATION_CLASSES': [ 
+        'rest_framework.authentication.SessionAuthentication', 
+        'rest_framework.authentication.BasicAuthentication',
+    ], 
+    'DEFAULT_PERMISSION_CLASSES': [ 
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
