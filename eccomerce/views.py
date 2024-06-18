889cb26dc8933b05a1baf51201c1c9c4309746fa
LizandroBackend <codeversebackend@gmail.com>
2024-06-18 12:20:01 -0600

    fix: Resolving error auth api | ci: feat: Add superuser admin Django | Dev: Lizandro

--- eccomerce/views.py
+++ eccomerce/views.py
@@ -1,24 +1,24 @@
 from rest_framework import generics 
 from .models import Category, Product 
 from .serializers import CategorySerializer, ProductSerializer 
-from rest_framework.permissions import IsAuthenticated 
+from rest_framework.permissions import AllowAny 
  
 class CategoryList(generics.ListCreateAPIView): 
     queryset = Category.objects.all() 
     serializer_class = CategorySerializer  
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
      
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView): 
     queryset = Category.objects.all() 
     serializer_class = CategorySerializer  
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
      
 class ProductList(generics.ListCreateAPIView): 
     queryset = Product.objects.all() 
     serializer_class = ProductSerializer 
-    permission_classes = [IsAuthenticated] 
+    permission_classes = [AllowAny] 
      
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView): 
     queryset = Product.objects.all() 
     serializer_class = ProductSerializer 
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
